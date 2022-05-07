bbf1d5fe8abd7dc6c00a218549b8bbf2138a999d
Nick <64551534+null-nick@users.noreply.github.com>
2022-05-07 16:10:41 +0200

    Update Pyrogram to v2.0.6
    Update to Pyrogram v2.0.6 from the latest commit on the official repo https://github.com/pyrogram/pyrogram/

--- pyrogram/types/messages_and_media/message.py
+++ pyrogram/types/messages_and_media/message.py
@@ -3322,7 +3322,7 @@ class Message(Object, Update):
 
             await client.send_reaction(
                 chat_id=chat_id,
-                message_id=message.message_id,
+                message_id=message.id,
                 emoji="🔥"
             )
 
@@ -3345,7 +3345,7 @@ class Message(Object, Update):
 
         return await self._client.send_reaction(
             chat_id=self.chat.id,
-            message_id=self.message_id,
+            message_id=self.id,
             emoji=emoji
         )
 
