59b212c9afad983dba16ad707b3a67426f32a74e
Roj <rojserbest@icloud.com>
2023-04-26 20:51:52 +0300

    Fix type hint for `InputMediaVideo`’s `thumb` attribute
    The thumb attribute can also be a `BinaryIO` as it is [passed](https://github.com/pyrogram/pyrogram/blob/master/pyrogram/methods/messages/send_media_group.py#L165) to a method in a position that can also accept a `BinaryIO`.
    
    Co-authored-by: Davide Galilei <DavideGalilei+43778739@users.noreply.github.com>

--- pyrogram/types/input_media/input_media_video.py
+++ pyrogram/types/input_media/input_media_video.py
@@ -35,7 +35,7 @@ class InputMediaVideo(InputMedia):
             pass a binary file-like object with its attribute “.name” set for in-memory uploads or
             pass an HTTP URL as a string for Telegram to get a video from the Internet.
 
-        thumb (``str``):
+        thumb (``str`` | ``BinaryIO``):
             Thumbnail of the video sent.
             The thumbnail should be in JPEG format and less than 200 KB in size.
             A thumbnail's width and height should not exceed 320 pixels.
