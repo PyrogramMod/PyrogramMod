8d3190f48e8071630de8b052e177f4742f7a7a72
shriMADhav U k <hetzner@shrimadhavuk.me>
2024-04-02 19:41:28 +0200

    Automate error scrapping
    Credits: https://t.me/c/1999755950/19474
    thanks to https://github.com/TelegramPlayGround/pyrogram/commit/c67504c7c930ed882988715a2c2fa60a25c8d081

--- compiler/errors/sort.py
+++ compiler/errors/sort.py
@@ -18,18 +18,69 @@
 
 import csv
 from pathlib import Path
+import re
+import requests  # requests==2.28.1
+import sys
 
-for p in Path("source").glob("*.tsv"):
-    with open(p) as f:
-        reader = csv.reader(f, delimiter="\t")
-        dct = {k: v for k, v in reader if k != "id"}
-        keys = sorted(dct)
+if len(sys.argv) != 2:
+    sys.exit(1)
 
-    with open(p, "w") as f:
-        f.write("id\tmessage\n")
+if sys.argv[1] == "sort":
+    for p in Path("source").glob("*.tsv"):
+        with open(p) as f:
+            reader = csv.reader(f, delimiter="\t")
+            dct = {k: v for k, v in reader if k != "id"}
+            keys = sorted(dct)
 
-        for i, item in enumerate(keys, start=1):
-            f.write(f"{item}\t{dct[item]}")
+        with open(p, "w") as f:
+            f.write("id\tmessage\n")
 
-            if i != len(keys):
-                f.write("\n")
+            for i, item in enumerate(keys, start=1):
+                f.write(f"{item}\t{dct[item]}")
+
+                if i != len(keys):
+                    f.write("\n")
+
+elif sys.argv[1] == "scrape":
+    b = "https://core.telegram.org"
+    c = "/api/errors"
+    a = requests.get(b + c)
+    d = a.text
+    e = r"\<a\ href\=\"(.*)\"\>here.*\<\/a\>"
+    f = re.search(e, d)
+    if f:
+        a = requests.get(
+            b + f.group(1)
+        )
+        d = a.json()
+        e = d.get("errors", [])
+        for h in e:
+            dct = {}
+
+            j = d.get("errors").get(h)
+            for k in j:
+                if k.endswith("_*"):
+                    continue
+                g = d.get("descriptions")
+                l = g.get(k)
+                m = k.replace("_%d", "_X")
+                l = l.replace("%d", "{value}")
+                l = l.replace("&raquo;", "»")
+                l = l.replace("&laquo;", "«")
+                l = l.replace("](/api/", f"]({b}/api/")
+                dct[m] = l
+
+            for p in Path("source/").glob(f"{h}*.tsv"):
+                with open(p) as f:
+                    reader = csv.reader(f, delimiter="\t")
+                    for k, v in reader:
+                        if k != "id":
+                            dct[k] = v
+
+            keys = sorted(dct)
+
+            for p in Path("source/").glob(f"{h}*.tsv"):
+                with open(p, "w") as f:
+                    f.write("id\tmessage\n")
+                    for i, item in enumerate(keys, start=1):
+                        f.write(f"{item}\t{dct[item]}\n")
