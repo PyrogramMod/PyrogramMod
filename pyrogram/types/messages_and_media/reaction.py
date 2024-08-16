82d9fd0388b789718adb30d9b1ae00b4ea88c609
shriMADhav U k <hetzner@shrimadhavuk.me>
2024-08-16 08:45:25 +0200

    Fix (iabw) parsing reactions

--- pyrogram/types/messages_and_media/reaction.py
+++ pyrogram/types/messages_and_media/reaction.py
@@ -39,6 +39,10 @@ class Reaction(Object):
         chosen_order (``int``, *optional*):
             Chosen reaction order.
             Available for chosen reactions.
+
+        is_paid (``bool``, *optional*):
+            True, if this is a paid reaction.
+
     """
 
     def __init__(
@@ -48,7 +52,8 @@ class Reaction(Object):
         emoji: Optional[str] = None,
         custom_emoji_id: Optional[int] = None,
         count: Optional[int] = None,
-        chosen_order: Optional[int] = None
+        chosen_order: Optional[int] = None,
+        is_paid: Optional[bool] = None
     ):
         super().__init__(client)
 
@@ -56,6 +61,7 @@ class Reaction(Object):
         self.custom_emoji_id = custom_emoji_id
         self.count = count
         self.chosen_order = chosen_order
+        self.is_paid = is_paid
 
     @staticmethod
     def _parse(
@@ -74,6 +80,12 @@ class Reaction(Object):
                 custom_emoji_id=reaction.document_id
             )
 
+        if isinstance(reaction, raw.types.ReactionPaid):
+            return Reaction(
+                client=client,
+                is_paid=True
+            )
+
         return None
 
     @staticmethod
@@ -86,7 +98,7 @@ class Reaction(Object):
 
         reaction = Reaction._parse(client, reaction_count.reaction)
         if reaction:
-            reaction.count = reaction_count.count
+            reaction.count = reaction_count.count if reaction_count else 0
             reaction.chosen_order = reaction_count.chosen_order
 
         return reaction
