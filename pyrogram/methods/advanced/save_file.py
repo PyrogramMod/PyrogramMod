583ce6d7ce2c79b73d98da8a09882e2b4d20db84
nick <64551534+null-nick@users.noreply.github.com>
2024-09-15 23:34:09 +0200

    save_files fixes

--- pyrogram/methods/advanced/save_file.py
+++ pyrogram/methods/advanced/save_file.py
@@ -128,10 +128,7 @@ class SaveFile:
             if file_size == 0:
                 raise ValueError("File size equals to 0 B")
 
-        # TODO
-        file_size_limit_mib = 2000
-        if self.me and self.me.is_premium:
-            file_size_limit_mib = 4000
+            file_size_limit_mib = 4000 if self.me and self.me.is_premium else 2000
 
             if file_size > file_size_limit_mib * 1024 * 1024:
                 raise ValueError(f"Can't upload files bigger than {file_size_limit_mib} MiB")
